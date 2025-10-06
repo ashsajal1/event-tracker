fc757afe016be3b3c3a24163447968d5a0fa732b
Ashsajal <ashsajal1@gmail.com>
2025-10-06 22:19:21 +0600

    feat: create event store

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,32 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+interface Events {
+  id: string
+  name: string
+  description: string
+  date: string
+  priority: 'high' | 'medium' | 'low'
+}
+
+export const useEventStore = defineStore('events', () => {
+  const events = ref<Events[]>([])
+
+  const createEvent = (event: Events) => {
+    events.value.push(event)
   }
 
-  return { count, doubleCount, increment }
+  const deleteEvent = (id: string) => {
+    events.value = events.value.filter((event) => event.id !== id)
+  }
+
+  const updateEvent = (id: string, event: Events) => {
+    events.value = events.value.map((event) => (event.id === id ? event : event))
+  }
+
+  const getEventById = (eventId: string) => {
+    return events.value.find((event) => event.id === eventId)
+  }
+
+  return { events, createEvent, deleteEvent, updateEvent, getEventById }
 })
