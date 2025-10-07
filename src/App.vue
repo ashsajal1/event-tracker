c62cfd2b383e2f5d02016eb0b86adf9897191e1a
Ashsajal <ashsajal1@gmail.com>
2025-10-07 16:51:48 +0600

    feat: add NavMenu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavMenu from './components/NavMenu.vue'
+</script>
 
 <template>
   <main class="main">
-    <header>Event tracker</header>
+    <NavMenu />
 
     <RouterView />
   </main>
 </template>
 
 <style lang="scss" scoped>
-
 .main {
   display: flex;
   flex-direction: column;
