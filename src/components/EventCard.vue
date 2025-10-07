627261eecae6adaa8c7c5f1d672e63b768e8ceba
Ashsajal <ashsajal1@gmail.com>
2025-10-07 12:05:58 +0600

    feat: add extra info

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>{{ event.name }}</div>
+  <div class="event-card">
+    <h3>{{ event.name }}</h3>
+    <div class="meta">
+      <p class="date">{{ event.date }}</p>
+      <p class="priority">{{ event.priority }}</p>
+    </div>
+    <p class="description">{{ event.description }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,4 +17,25 @@ defineProps<{
 }>()
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.event-card {
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 1rem;
+
+  .meta {
+    display: flex;
+    gap: 2rem;
+    margin-bottom: 0.5rem;
+    align-items: center;
+  }
+
+  h3 {
+    margin-bottom: 0.5rem;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+}
+</style>
