812e900c031a97fbe0c8ab118c14a01d292091a7
Ashsajal <ashsajal1@gmail.com>
2025-10-07 16:51:36 +0600

    feat: create NavMenu with toggle mode

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <h3>Logo</h3>
+    <div>
+      <button @click="toggleDark()">{{ isDark ? 'Light' : 'Dark' }}</button>
+    </div>
+  </nav>
+</template>
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+<style lang=""></style>
