af42245b7f9eae4b2163bcf4b1e1c39c53072543
Ashsajal <ashsajal1@gmail.com>
2025-10-07 16:34:48 +0600

    style: make form sticky

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,42 @@
 <script setup lang="ts">
-import EventForm from '@/components/EventForm.vue';
+import EventForm from '@/components/EventForm.vue'
 import { useEventStore } from '../stores/events'
-import EventCard from '@/components/EventCard.vue';
+import EventCard from '@/components/EventCard.vue'
 const eventStore = useEventStore()
 const events = eventStore.events
 </script>
 
 <template>
-  <main>
-    <h1>Events</h1>
-    <template v-for="event in events" :key="event.id">
-      <EventCard :event="event" />
-    </template>
+  <main class="container">
+    <section class="events-section">
+      <h1>Events</h1>
+      <template v-for="event in events" :key="event.id">
+        <EventCard :event="event" />
+      </template>
+    </section>
 
-    <EventForm />
+    <section class="form-section">
+      <EventForm />
+    </section>
   </main>
 </template>
 
 <style scoped lang="scss">
-main {
+.container {
   display: flex;
+  justify-content: space-between;
+  align-items: start;
+  width: 100%;
+
+  section {
+    width: 100%;
+    margin: 12px;
+  }
+
+  .form-section {
+    width: 400px;
+    position: sticky;
+    top: 20px;
+  }
 }
 </style>
