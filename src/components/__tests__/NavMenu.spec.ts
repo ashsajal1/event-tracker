c0adf92fef72ce9de7a30f284aa97ea5a24c9219
Ashsajal <ashsajal1@gmail.com>
2025-10-09 18:29:30 +0600

    test: write toggle mock

--- src/components/__tests__/NavMenu.spec.ts
+++ src/components/__tests__/NavMenu.spec.ts
@@ -1,11 +1,26 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import NavMenu from "../NavMenu.vue"
+import NavMenu from '../NavMenu.vue'
+
+vi.mock('@vueuse/core', () => ({
+  useDark: vi.fn(() => ({
+    value: false,
+  })),
+  useToggle: vi.fn((isDark) => {
+    const toggle = () => {
+      isDark.value = !isDark.value
+
+      //simulate dom
+      document.documentElement.setAttribute('data-theme', isDark.value ? 'dark' : 'light')
+    }
+    return [isDark.value, toggle]
+  }),
+}))
 
 describe('NavMenu component', () => {
   it('should render properly render', () => {
-    const wrapper = mount(NavMenu);
-    expect(wrapper.exists()).toBe(true);
+    const wrapper = mount(NavMenu)
+    expect(wrapper.exists()).toBe(true)
   })
- })
+})
