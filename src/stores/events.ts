658722bcf215478b1058f9a2a02525c212d06061
Ashsajal <ashsajal1@gmail.com>
2025-10-09 18:09:46 +0600

    refactor: name of state

--- src/stores/events.ts
+++ src/stores/events.ts
@@ -11,23 +11,23 @@ export interface Event {
 }
 
 export const useEventtore = defineStore('Event', () => {
-  const Event = ref<Event[]>([])
+  const events = ref<Event[]>([])
 
   const createEvent = (event: Omit<Event, 'id'>) => {
-    Event.value.push({ ...event, id: uuid() })
+    events.value.push({ ...event, id: uuid() })
   }
 
   const deleteEvent = (id: string) => {
-    Event.value = Event.value.filter((event) => event.id !== id)
+    events.value = events.value.filter((event) => event.id !== id)
   }
 
   const updateEvent = (id: string, event: Event) => {
-    Event.value = Event.value.map((event) => (event.id === id ? event : event))
+    events.value = events.value.map((ev) => (ev.id === id ? event : ev))
   }
 
   const getEventById = (eventId: string) => {
-    return Event.value.find((event) => event.id === eventId)
+    return events.value.find((event) => event.id === eventId)
   }
 
-  return { Event, createEvent, deleteEvent, updateEvent, getEventById }
+  return { events, createEvent, deleteEvent, updateEvent, getEventById }
 })
