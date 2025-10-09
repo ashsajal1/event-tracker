99fc38c33bddee5c96da92a8d1ded485088c2f5b
Ashsajal <ashsajal1@gmail.com>
2025-10-09 18:39:13 +0600

    test: write toggle test

--- src/components/__tests__/NavMenu.spec.ts
+++ src/components/__tests__/NavMenu.spec.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import NavMenu from '../NavMenu.vue'
+import { Moon, Sun } from 'lucide-vue-next'
 
 vi.mock('@vueuse/core', () => ({
   useDark: vi.fn(() => ({
@@ -22,5 +23,37 @@ describe('NavMenu component', () => {
   it('should render properly render', () => {
     const wrapper = mount(NavMenu)
     expect(wrapper.exists()).toBe(true)
+  })
+
+  let wrapper: ReturnType<typeof mount>
+
+  const createWrapper = () => {
+    return mount(NavMenu, {
+      global: {
+        stubs: {
+          Moon,
+          Sun,
+        },
+      },
+    })
+  }
+
+  beforeEach(() => {
+    // Reset dom
+    document.documentElement.removeAttribute('data-theme')
+    wrapper = createWrapper()
+  })
+
+  it('renders moon icon in light mode', () => {
+    const moonIcon = wrapper.findComponent(Moon)
+    expect(moonIcon.exists()).toBe(true)
+  })
+
+  it('toggles dark and light mode', async () => {
+    const button = wrapper.find('button')
+
+    await button.trigger('click')
+
+    expect(document.documentElement.getAttribute('data-theme')).toBe('dark')
   })
 })
