71752e6fa2c446f30803ee02f092c1797882d279
Ashsajal <ashsajal1@gmail.com>
2025-10-10 14:41:47 +0600

    feat: create ErrorText.vue

---
+++ src/components/ErrorText.vue
@@ -0,0 +1,15 @@
+<template>
+  <p v-if="error" class="error">{{ error }}</p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  error: string
+}>()
+</script>
+
+<style scoped>
+.error {
+  color: rgb(211, 58, 58);
+}
+</style>
