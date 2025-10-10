78f3696d0145dc73ca7875fb5f2da768d548a969
Ashsajal <ashsajal1@gmail.com>
2025-10-10 14:40:57 +0600

    feat: add form validation

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,9 +1,12 @@
 <template>
-  <form @submit.prevent="addEvent">
-    <input v-model="newEvent.name" type="text" placeholder="Event name" />
-    <textarea v-model="newEvent.description" placeholder="Event description" />
-    <input v-model="newEvent.date" type="date" placeholder="Event date" />
-    <select v-model="newEvent.priority">
+  <form @submit.prevent="onSubmit">
+    <input v-model="name" v-bind="nameAttr" type="text" placeholder="Event name" />
+    <ErrorText :error="errors.name!" />
+    <textarea v-model="description" v-bind="descriptionAttr" placeholder="Event description" />
+    <ErrorText :error="errors.description!" />
+    <input v-model="date" v-bind="dateAttr" type="date" placeholder="Event date" />
+    <ErrorText :error="errors.date!" />
+    <select v-model="priority" v-bind="priorityAttr">
       <option value="high">High</option>
       <option value="medium">Medium</option>
       <option value="low">Low</option>
@@ -13,24 +16,32 @@
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
 import { useEventStore, type Event } from '../stores/events'
 const eventStore = useEventStore()
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import ErrorText from './ErrorText.vue'
+import z from 'zod'
 
-const newEvent = reactive<Omit<Event, 'id'>>({
-  name: '',
-  description: '',
-  date: new Date().toISOString().split('T')[0]!,
-  priority: 'high',
+const eventSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  description: z.string().min(1, 'Description is required'),
+  date: z.string().min(1, 'Date is required'),
+  priority: z.enum(['high', 'medium', 'low']),
 })
 
-const addEvent = () => {
-  newEvent.date = new Date().toISOString().split('T')[0]!
-  eventStore.createEvent(newEvent)
-  newEvent.name = ''
-  newEvent.description = ''
-  newEvent.priority = 'high'
-}
+const { handleSubmit, errors, defineField } = useForm({
+  validationSchema: toTypedSchema(eventSchema),
+})
+
+const [name, nameAttr] = defineField('name')
+const [description, descriptionAttr] = defineField('description')
+const [date, dateAttr] = defineField('date')
+const [priority, priorityAttr] = defineField('priority')
+
+const onSubmit = handleSubmit((values) => {
+  eventStore.createEvent(values as Omit<Event, 'id'>)
+})
 </script>
 
 <style>
